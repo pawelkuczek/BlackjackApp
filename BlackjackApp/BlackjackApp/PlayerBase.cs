578dfa34f2fcedd9745bdfc9a1b4b5d4b69b9d02
Paweł Kuczek <pawel.kuczek@vp.pl>
2023-07-27 13:32:06 +0200

    Add class PlayerBase

---
+++ BlackjackApp/BlackjackApp/PlayerBase.cs
@@ -0,0 +1,19 @@
+﻿namespace BlackjackApp
+{
+    public abstract class PlayerBase : IPlayer
+    {
+        public PlayerBase(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+
+        public abstract void CalculatePoints();
+
+        public abstract void Pass();
+
+
+        public abstract void TakeCard();
+       
+    }
+}
