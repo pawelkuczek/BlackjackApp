8fac23f8cc56838d53c0d07008625e6dc5a455e3
Paweł Kuczek <pawel.kuczek@vp.pl>
2023-07-27 22:49:19 +0200

    Add Croupier class with ShuffleCards method

---
+++ BlackjackApp/BlackjackApp/Croupier.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace BlackjackApp
+{
+    public class Croupier : PlayerBase
+    {
+        public Croupier(string name) : base(name) { }
+
+        public List<Card> ShuffleCards(List<Card> cards)
+        {
+            var random = new Random();
+            var ShuffledCards = new List<Card>();
+            var listcCount = cards.Count;
+            for (int i = 0; i < listcCount; i++)
+            {
+                var randomElementInList = random.Next(0, cards.Count);
+                ShuffledCards.Add(cards[randomElementInList]);
+                cards.Remove(cards[randomElementInList]);
+            }
+            return ShuffledCards;
+        }
+
+        public override void CalculatePoints()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Pass()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void TakeCard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BlackjackApp/BlackjackApp/Program.cs
+++ BlackjackApp/BlackjackApp/Program.cs
@@ -4,8 +4,17 @@ using System.Collections.Generic;
 var deck = new Deck();
 deck.CreateDeckOfCards();
 
+var croupier = new Croupier("Croupier");
+var shuffledCards = croupier.ShuffleCards(deck.cards);
 
-foreach (var card in deck.cards)
+foreach (var shuffledCard in shuffledCards)
 {
-    Console.WriteLine($"{card.Value} - {card.Color}");
+    Console.WriteLine($"{shuffledCard.Value} - {shuffledCard.Color}");
 }
+
+//foreach (var card in deck.cards)
+//{
+//    Console.WriteLine($"{card.Value} - {card.Color}");
+//}
+
+
