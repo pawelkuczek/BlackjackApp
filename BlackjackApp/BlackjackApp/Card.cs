151bd3d0fd0055e2c4618932593d06c6af7dce84
Paweł Kuczek <pawel.kuczek@vp.pl>
2023-07-27 12:03:26 +0200

    Create class Card

---
+++ BlackjackApp/BlackjackApp/Card.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace BlackjackApp
+{
+    public class Card
+    {
+        public Card(string color, string value)
+        {
+            this.Color = color;
+            this.Value = value;
+        }
+
+        public string Color { get; private set; }
+
+        public string Value { get; private set; }
+
+
+    }
+}
