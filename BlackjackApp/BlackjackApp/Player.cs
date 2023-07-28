4625f2e048677b7794dd20a9a5a82d2de2165f36
Paweł Kuczek <pawel.kuczek@vp.pl>
2023-07-28 14:57:47 +0200

    Add player class

---
+++ BlackjackApp/BlackjackApp/Player.cs
@@ -0,0 +1,22 @@
+﻿namespace BlackjackApp
+{
+    public class Player : PlayerBase
+    {
+        public Player(string name) : base (name) {}
+      
+        public override void CalculatePoints()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Pass()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void TakeCard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BlackjackApp/BlackjackApp/PlayerBase.cs
+++ BlackjackApp/BlackjackApp/PlayerBase.cs
@@ -2,9 +2,12 @@
 {
     public abstract class PlayerBase : IPlayer
     {
+         public List<Card> playerCards = new List<Card>();
+
         public PlayerBase(string name)
         {
             this.Name = name;
+            
         }
         public string Name { get; set; }
 
